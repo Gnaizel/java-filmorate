759f3f3d53902ec73d54a14cb95cdc8e56ccd06a
Gnaizel <Gnaizel@mail.ru>
2024-11-30 05:39:52 +0300

    fix popularFilm

--- src/main/java/gnaizel/inc/controller/GenreController.java
+++ src/main/java/gnaizel/inc/controller/GenreController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/genre")
+@RequestMapping("/genres")
 public class GenreController {
     GenreDB genreDB;
 
--- src/main/java/gnaizel/inc/controller/MpaController.java
+++ src/main/java/gnaizel/inc/controller/MpaController.java
@@ -2,11 +2,15 @@ package gnaizel.inc.controller;
 
 import gnaizel.inc.enums.film.MPA;
 import gnaizel.inc.storage.mpa.impl.MpaDbStorage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
+@RestController
 @RequestMapping("/mpa")
 public class MpaController {
     MpaDbStorage mpaDbStorage;
@@ -18,6 +22,7 @@ public class MpaController {
 
     @GetMapping("/{id}")
     public MPA getMpaById(int id) {
+        log.info(String.valueOf(id));
         return mpaDbStorage.findGMpaById(id);
     }
 }
