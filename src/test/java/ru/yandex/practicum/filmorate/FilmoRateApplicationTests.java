5d7481a7d3ba94c469af88fabdd0a0c50b70bcc0
Gnaizel <Gnaizel@mail.ru>
2024-12-01 06:51:24 +0300

    reformat code(check style)

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -1,17 +1,12 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.storage.user.impl.UserDbStorage;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase
